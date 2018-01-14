97da3a447be27b5f3d71ec8fec6b02e33828a0d4
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 13:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function add(a=1,b=1){return a+b}
 function subtract(a=1,b=1){return a-b}
 function multiply(a=2,b=4){return a*b}
 function divide(a=2,b=2){return a/b}
-function
+function inc(n=5){return ++n}
+function dec(n=5){return --n}
+function makeInt(n=5){return parseInt(n)}
